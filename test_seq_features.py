bea438a02da4d8fcf6fb6ac27fdf7a517b31dbdd
treweek <treweek@caltech.edu>
2017-06-23 15:22:18 -0700

    commit new seq_features files

--- test_seq_features.py
+++ test_seq_features.py
@@ -1,4 +1,5 @@
 import seq_features
+import pytest
 
 def test_n_neg():
     """Perform unit tests on seq_features.n_neg."""
@@ -9,3 +10,8 @@ def test_n_neg():
     assert seq_features.n_neg('ACKLWTTAE') == 1
     assert seq_features.n_neg('DDDDEEEE') == 8
     assert seq_features.n_neg('acklwttae') == 1
+
+def test_n_neg_for_invalid_amino_acid():
+    with pytest.raises(RuntimeError) as excinfo:
+        seq_features.n_neg('Z')
+    excinfo.match("Z is not a valid amino acid")
